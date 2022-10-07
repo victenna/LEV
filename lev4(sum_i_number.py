18450a2f4e666be0988e66c4cdeb42cd7afc791e
victenna <39585970+victenna@users.noreply.github.com>
2022-10-07 13:23:13 -0400

    Add files via upload

---
+++ lev4(sum_i_number.py
@@ -0,0 +1,13 @@
+#The average of n integer numbers from n1 to n2
+
+n1= 1
+n2=10
+sum=0
+N=0
+for i in range(n1,n2+1):
+    
+    sum=sum+i
+    print(sum)
+#print('sum=',sum)
+average=sum/(n2-n1+1)
+print(average)
