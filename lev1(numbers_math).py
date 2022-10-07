dd1ed0e2ddb10771fefe1bdeb36e84b793064341
victenna <39585970+victenna@users.noreply.github.com>
2022-10-07 13:22:05 -0400

    Add files via upload

---
+++ lev1(numbers_math).py
@@ -0,0 +1,24 @@
+import math
+a=5.654 # float
+b=45 # degree
+c=0.785
+print('#1',math.sqrt(a))
+print('#2',round(math.sqrt(a),1))
+print('#3',math.pow(a,3))
+print('#4',math.radians(b))
+print('#5',math.pi)
+print('#6',math.radians(45))
+print('#7',math.degrees(c))
+print('#8',round(math.sin(math.radians(30)),1))
+print('#9',round(math.cos(math.radians(60)),1))
+print('#10',round(math.tan(math.radians(45)),1))
+print('11',round(math.cos(math.radians(60)),1))
+print('12',math.e)
+print('13',abs(-4))
+z=complex(5,5)
+print('14',z.real,z.imag)
+fi=math.atan(z.imag/z.real)
+print('14',math.degrees(fi))
+
+d=73.876901356
+print(round(d,2))
