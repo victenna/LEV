591530e03e8c3cf2eadff464797d15036a7cad87
victenna <39585970+victenna@users.noreply.github.com>
2022-10-07 13:28:08 -0400

    Add files via upload

---
+++ lev7(string ).py
@@ -0,0 +1,16 @@
+a='Lev'
+b='Lev am 20 years old'
+c='Victor'
+d='Victor is 65 years old'
+e='Lev and Victor together are'
+f='years old'
+
+print(len(a+b))
+print(e,'85',f)
+print(e,85,f)
+
+g='20'
+h='65'
+print(g+h)
+print(int(g)+int(h))
+
